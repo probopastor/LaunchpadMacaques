5525b9605dc1414ee18cb51a9159e7a12f9163bf
probopastor <43503592+probopastor@users.noreply.github.com>
2020-09-03 14:29:36 -0500

    Added Updated Trial and Error Project
    Updated old prototype to new unity version, added it to project. It is what we will work off of now.

---
+++ Trial & Error/Assets/TE_Scripts/Luke Scripts/PushPull.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PushPull : MonoBehaviour
+{
+    public LayerMask wallLayer;
+
+    private Rigidbody _rigidbody;
+    private Camera _camera;
+    
+    [SerializeField]
+    private GameObject pullPos;
+    
+    [SerializeField]
+    private GameObject pushParticle;
+    private ParticleSystem _pullParticle;
+    [SerializeField]
+    private float startPullParticleSpeed = 5;
+    [SerializeField]
+    private float pullingParticleSpeed = 50;
+    
+    public float pullSpeed = 60f;
+    public float pushSpeed = 60f;
+
+    [SerializeField]
+    private float pullMaxDistance = 10f;
+    [SerializeField]
+    private float pushMaxDistance = 5f;
+
+    [SerializeField]
+    private Image crossHairUI;
+    [SerializeField]
+    private List<Sprite> crossHairSprites = new List<Sprite>();
+
+    [SerializeField]
+    private CinemachineVirtualCamera playerCam;
+    private float _startCamFOV = 60;
+    [SerializeField]
+    private float pullCamFov = 90;
+    [SerializeField]
+    private float pushCamFov = 90;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _camera = Camera.main;
+        playerCam.m_Lens.FieldOfView = _startCamFOV;
+        _pullParticle = pullPos.GetComponentInChildren<ParticleSystem>();
+        _pullParticle.Stop();
+        crossHairUI.sprite = crossHairSprites[0];
+    }
+
+   
+    void Update()
+    {
+        var ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+        var pullParticleMain = _pullParticle.main;
+        if (Physics.Raycast(ray, out RaycastHit particleHit, pullMaxDistance, wallLayer))
+        {
+            pullPos.transform.position = particleHit.point;
+            pullPos.transform.LookAt(transform.position);
+            _pullParticle.Play();
+        }
+        else
+        {
+            _pullParticle.Stop();
+        }
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray, out RaycastHit pullHit, pullMaxDistance, wallLayer))
+            {
+                pullPos.transform.position = pullHit.point;
+                pullPos.transform.parent = pullHit.transform;
+                pullParticleMain.startSpeed = pullingParticleSpeed;
+                crossHairUI.sprite = crossHairSprites[1];
+            }
+            else
+            {
+                pullPos.transform.parent = transform;
+                pullPos.transform.position = transform.position;
+            }
+        }
+        
+        //pull mechanic
+        if (Input.GetMouseButton(0))
+        {
+            if (pullPos.transform.parent != transform) //&& Physics.Raycast(ray, out RaycastHit hit, pullMaxDistance, wallLayer))
+            {
+                //_rigidbody.position = Vector3.Lerp(_rigidbody.position, pullPos.transform.position, Time.deltaTime * pullSpeed);
+
+                var forceDir = pullPos.transform.position - transform.position;
+                _rigidbody.AddForce(forceDir * pullSpeed, ForceMode.Acceleration);
+                
+                if (playerCam.m_Lens.FieldOfView < pullCamFov)
+                {
+                    playerCam.m_Lens.FieldOfView += Time.deltaTime * 30f;
+                }
+            }
+            else if (playerCam.m_Lens.FieldOfView > _startCamFOV)
+            {
+                playerCam.m_Lens.FieldOfView -= Time.deltaTime * 50f;
+            }
+        }
+        else if (playerCam.m_Lens.FieldOfView > _startCamFOV)
+        {
+            playerCam.m_Lens.FieldOfView -= Time.deltaTime * 50f;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            pullParticleMain.startSpeed = startPullParticleSpeed;
+            crossHairUI.sprite = crossHairSprites[0];
+        }
+
+
+        //push mechanic
+        if (Input.GetMouseButtonDown(1))
+        {
+            //shoots a raycast out at X and checks if it hits something on the wallLayer
+            if (Physics.Raycast(ray, out RaycastHit hit, pushMaxDistance, wallLayer))
+            {
+                //pushes the player in the opposite direction of the hit Raycast
+                _rigidbody.AddForce(-ray.direction * pushSpeed, ForceMode.Impulse);
+                var newPushParticle = Instantiate(pushParticle, hit.point, Quaternion.identity);
+                newPushParticle.transform.LookAt(transform.position);
+                playerCam.m_Lens.FieldOfView = pushCamFov;
+                crossHairUI.sprite = crossHairSprites[2];
+            }
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            crossHairUI.sprite = crossHairSprites[0];
+        }
+    }
+}
---
+++ Trial & Error/Assets/TE_Scripts/PlayerMovement.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    private Transform _mainCameraRotation;
+
+    [SerializeField]
+    private float playerSpeed = 1f;
+    [SerializeField, Tooltip("Max speed the player can move at")]
+    private float maxPlayerSpeed = 8f;
+    [SerializeField]
+    private float maxAirSpeed = 10f;
+    private Vector2 _movementDir = Vector2.zero;
+    [SerializeField] private float jumpForce = 10;
+    private bool _inAir = false;
+    
+    private int _collectiblesCollected = 0;
+    private int _totalCollectibles;
+    public TextMeshProUGUI sphereText;
+    private Vector3 _startingPosition;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _mainCameraRotation = FindObjectOfType<CinemachineBrain>().transform;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        _totalCollectibles = GameObject.FindGameObjectsWithTag("Collectible").Length;
+        sphereText.text = "Collected Spheres: " + _collectiblesCollected + " / " + _totalCollectibles;
+        _startingPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        MoveToSpawn();
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (!_inAir && Physics.Raycast(transform.position, Vector3.down, 1f))
+        {
+            _inAir = false;
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        _inAir = true;
+    }
+
+    void MovePlayer()
+    {
+        Vector3 velocity = _rigidbody.velocity;
+        var camRot = _mainCameraRotation.rotation;
+        camRot.eulerAngles = new Vector3(0, camRot.eulerAngles.y, 0);
+        if (_inAir)
+        {
+            velocity += camRot * Vector3.ClampMagnitude(new Vector3(_movementDir.x, 0, _movementDir.y) * playerSpeed, maxAirSpeed);
+        }
+        else
+        {
+            velocity += camRot * Vector3.ClampMagnitude(new Vector3(_movementDir.x, 0, _movementDir.y) * playerSpeed, maxPlayerSpeed);
+        }
+        velocity = new Vector3(velocity.x, _rigidbody.velocity.y, velocity.z);
+        _rigidbody.velocity = velocity;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Collectible"))
+        {
+            //Debug.Log("Got it!");
+            _collectiblesCollected++;
+            Destroy(other.gameObject);
+            sphereText.text = "Collected Spheres: " + _collectiblesCollected + " / " + _totalCollectibles;
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        _movementDir = context.performed ? context.ReadValue<Vector2>() : Vector2.zero;
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, 1f))
+        {
+            _rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void MoveToSpawn()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            _rigidbody.isKinematic = true;
+            transform.position = _startingPosition;
+            _rigidbody.isKinematic = false;
+        }
+    }
+}
