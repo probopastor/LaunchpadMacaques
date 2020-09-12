ae6527c25f1a53b371e9ee4762f1d03bbc28587d
probopastor <43503592+probopastor@users.noreply.github.com>
2020-09-11 20:34:14 -0500

    Updated Decals
    -Decal prefab now instantiated on game start

--- LaunchpadMacaques_Capstone/Assets/Scripts/GrappleDecal.cs
+++ LaunchpadMacaques_Capstone/Assets/Scripts/GrappleDecal.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GrappleDecal : MonoBehaviour
 {
@@ -9,10 +7,13 @@ public class GrappleDecal : MonoBehaviour
 
     private ConfigJoint configJoint;
 
+    private GameObject grappleDecalObj;
+
     // Start is called before the first frame update
     void Start()
     {
         configJoint = FindObjectOfType<ConfigJoint>();
+        grappleDecalObj = Instantiate(grappleAimingDecal);
     }
 
     // Update is called once per frame
@@ -28,27 +29,18 @@ public class GrappleDecal : MonoBehaviour
 
         if(Physics.Raycast(ray, out hitInfo, configJoint.GetMaxGrappleDistance(), whatIsGrappleable))
         {
-            grappleAimingDecal.SetActive(true);
+            grappleDecalObj.SetActive(true);
             MoveDecal(hitInfo);
         }
         else
         {
-            grappleAimingDecal.SetActive(false);
+            grappleDecalObj.SetActive(false);
         }
     }
 
     private void MoveDecal(RaycastHit info)
     {
-        //grappleAimingDecal.transform.rotation = Quaternion.FromToRotation(Vector3.right, info.normal);
-        //grappleAimingDecal.transform.rotation = new Quaternion(grappleAimingDecal.transform.rotation.x, grappleAimingDecal.transform.rotation.y, grappleAimingDecal.transform.rotation.z, grappleAimingDecal.transform.rotation.w);
-
-        //grappleAimingDecal.transform.position = info.point;
-        //grappleAimingDecal.transform.rotation = new Quaternion(info.normal.x, info.normal.y, info.normal.z, grappleAimingDecal.transform.rotation.w);
-
-        //if (Input.GetKeyDown(KeyCode.Alpha1))
-        //    Instantiate(grappleAimingDecal, info.point, Quaternion.FromToRotation(new Vector3(Vector3.up.x, Vector3.up.y, Vector3.up.z + 90), info.normal));
-
-        grappleAimingDecal.transform.position = info.point;
-        grappleAimingDecal.transform.rotation = Quaternion.FromToRotation(new Vector3(Vector3.up.x, Vector3.up.y, Vector3.up.z + 90), info.normal);
+        grappleDecalObj.transform.position = info.point;
+        grappleDecalObj.transform.rotation = Quaternion.FromToRotation(new Vector3(Vector3.up.x, Vector3.up.y, Vector3.up.z + 90), info.normal);
     }
 }
