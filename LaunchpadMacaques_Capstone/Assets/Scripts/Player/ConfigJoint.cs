a098b14d9100469bc587ef2a2e6861325888bc19
probopastor <43503592+probopastor@users.noreply.github.com>
2020-09-14 05:29:21 -0500

    ConfigJoint.cs Set Up With Push
    Combined necessary parts of both config joint versions.
    
    Current ConfigJoint.cs includes min/max joint times and pushing capabilities.

--- LaunchpadMacaques_Capstone/Assets/Scripts/Player/ConfigJoint.cs
+++ LaunchpadMacaques_Capstone/Assets/Scripts/Player/ConfigJoint.cs
@@ -9,9 +9,19 @@ public class ConfigJoint : MonoBehaviour
     public LayerMask whatIsGrappleable;
     private Vector3 currentGrapplePosition;
     public Transform gunTip, camera, player;
-    private float maxDistance = 100f;
+    private float maxPullDistance = 100f;
+    private float maxPushDistance = 20f;
     private ConfigurableJoint joint;
+    private RaycastHit grappleRayHit;
 
+
+    private enum GrappleType { Pull, Push }
+    //If the player is Pulling, this is the object they're pulling toward
+    private Transform currentGrappleTarget = null;
+    //Offset to account for the grapple target moving while pulling to it
+    private Vector3 currentGrappleTargetOffset = Vector3.zero;
+    //Whether the player is currently Grappling
+    private bool isGrappling = false;
 
     [Header("Explosion Settings")]
     public float explosionRadius = 5f;
@@ -25,18 +35,18 @@ public class ConfigJoint : MonoBehaviour
     SoftJointLimitSpring springLimit;
     JointDrive jointDrive;
 
-    [Header("Spring Settings")]
-    [SerializeField] float positionSpringForce = 350;
-    [SerializeField] float positionSpringDamper = 300;
+    [Header("Pull - Spring Settings")]
+    [SerializeField] float pullSpringForce = 350;
+    [SerializeField] float pullSpringDamper = 300;
 
-    private GameObject objectFixedTo;
-    private RaycastHit grappleRayHit;
+    [Header("Push - Spring Settings")]
+    [SerializeField] float pushSpringForce = 350;
+    [SerializeField] float pushSpringDamper = 300;
 
     [Header("Grapple Settings")]
     [Tooltip("The Min amount of time a joint has to connected before it can be discontented")]
     [SerializeField] float minJointTime = .08f;
     [SerializeField] bool grappleHasMax = false;
-
 
     [Tooltip("The Max amount of time a joint will be connected for")]
     [SerializeField] float maxJointTime = 2;
@@ -46,32 +56,48 @@ public class ConfigJoint : MonoBehaviour
         lr = GetComponent<LineRenderer>();
     }
 
-    private void Start()
-    {
-        SetSpringSettings();
-    }
-
-    private void SetSpringSettings()
+    /// <summary>
+    /// Adjust 
+    /// </summary>
+    /// <param name="grappleType"></param>
+    private void SetSpringSettings(GrappleType grappleType)
     {
         jointLimit = new SoftJointLimit();
         springLimit = new SoftJointLimitSpring();
         jointDrive = new JointDrive();
         jointDrive.maximumForce = 3.402823e+38f;
-        jointDrive.positionSpring = positionSpringForce;
-        jointDrive.positionDamper = positionSpringDamper;
-        lr.positionCount = 0;
+
+        if (grappleType == GrappleType.Pull)
+        {
+            jointDrive.positionSpring = pullSpringForce;
+            jointDrive.positionDamper = pullSpringDamper;
+        }
+        else
+        {
+            jointDrive.positionSpring = pushSpringForce;
+            jointDrive.positionDamper = pushSpringDamper;
+        }
     }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && joint == null)
+        if (Input.GetMouseButtonDown(0) && !isGrappling)
         {
-            StartGrapple();
-        }
 
-        if (Input.GetMouseButtonDown(1))
+            StartGrapple(GrappleType.Pull);
+        }
+        //else if (Input.GetMouseButtonUp(0))
+        //{
+        //    StopGrapple();
+        //}
+
+        if (Input.GetMouseButton(1) && !isGrappling)
         {
-            Explode();
+            StartGrapple(GrappleType.Push);
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            StopGrapple();
         }
     }
 
@@ -86,7 +112,7 @@ public class ConfigJoint : MonoBehaviour
     {
         print("Explode");
         RaycastHit hit;
-        if (Physics.Raycast(camera.position, camera.forward, out hit, maxDistance, whatIsGrappleable))
+        if (Physics.Raycast(camera.position, camera.forward, out hit, maxPullDistance, whatIsGrappleable))
         {
             Vector3 explosionPos = transform.position;
             Collider[] colliders = Physics.OverlapSphere(explosionPos, explosionRadius);
@@ -99,7 +125,6 @@ public class ConfigJoint : MonoBehaviour
                     rb.AddExplosionForce(explosionPower, explosionPos, explosionRadius, 0.0f, ForceMode.Impulse);
                 }
             }
-
         }
     }
 
@@ -107,35 +132,91 @@ public class ConfigJoint : MonoBehaviour
     /// <summary>
     /// Call whenever we want to start a grapple
     /// </summary>
-    void StartGrapple()
+    void StartGrapple(GrappleType grappleType)
     {
+
         RaycastHit hit;
-        if (Physics.Raycast(camera.position, camera.forward, out hit, maxDistance, whatIsGrappleable))
+        //If pushing and there is a surface in front of the player for them to push off of
+        if (grappleType == GrappleType.Push && Physics.Raycast(camera.position, camera.forward, out hit, maxPushDistance, ~LayerMask.GetMask("CantPush")))
         {
+            isGrappling = true;
+
+            lr.positionCount = 0;
+        }
+        //If pulling and there is a surface in front of the player in which they can grapple to
+        else if (grappleType == GrappleType.Pull && Physics.Raycast(camera.position, camera.forward, out hit, maxPullDistance, whatIsGrappleable))
+        {
+            isGrappling = true;
+
             grappleRayHit = hit;
 
-            objectFixedTo = hit.collider.gameObject;
+            //Set Grapple target and mark point to pull to
+            currentGrappleTarget = hit.collider.transform;
             grapplePoint = hit.point;
-            joint = player.gameObject.AddComponent<ConfigurableJoint>();
-            joint.autoConfigureConnectedAnchor = false;
+            currentGrappleTargetOffset = grapplePoint - currentGrappleTarget.position;
+
+            lr.positionCount = 2;
+            currentGrapplePosition = gunTip.position;
+
+            //StartCoroutine(JointDestroyDelay());
+        }
+
+        //Not able to pull or push
+        if (!isGrappling)
+            return;
+
+        //Joint setup based on push or pull
+        joint = player.gameObject.AddComponent<ConfigurableJoint>();
+        joint.autoConfigureConnectedAnchor = false;
+
+        SetSpringSettings(grappleType);
+        joint.xDrive = jointDrive;
+        joint.yDrive = jointDrive;
+        joint.zDrive = jointDrive;
+
+        StartCoroutine(UpdateGrapplePosition(grappleType));
+    }
+
+    /// <summary>
+    /// Coroutine that updates the position of the grapple anchor to the appropriate point and then updates the joint
+    /// </summary>
+    /// <param name="grappleType">Push or Pull</param>
+    /// <returns></returns>
+    IEnumerator UpdateGrapplePosition(GrappleType grappleType)
+    {
+        RaycastHit hit;
+
+        while (isGrappling)
+        {
+            //Pushing
+            if (grappleType == GrappleType.Push && Physics.Raycast(camera.position, camera.forward, out hit, maxPushDistance, ~LayerMask.GetMask("CantPush")))
+            {
+                //Update grapple point to arbitrary point behind player
+                grapplePoint = hit.point + -(camera.forward * maxPushDistance);
+            }
+            else if (grappleType == GrappleType.Push)
+            {
+                StopGrapple();
+            }
+            //Pulling
+            else if (grappleType == GrappleType.Pull)
+            {
+                //Update grapple point to the target + original offset
+                grapplePoint = currentGrappleTarget.position + currentGrappleTargetOffset;
+                StartCoroutine(JointDestroyDelay());
+            }
+
+
+            //Update Joint
             joint.connectedAnchor = grapplePoint;
 
             float distanceFromPoint = Vector3.Distance(player.position, grapplePoint);
             jointLimit.limit = distanceFromPoint;
             joint.targetPosition = Vector3.zero;
 
-
-            joint.xDrive = jointDrive;
-            joint.yDrive = jointDrive;
-            joint.zDrive = jointDrive;
-           
-          
-            lr.positionCount = 2;
             currentGrapplePosition = gunTip.position;
 
-            StartCoroutine(JointDestroyDelay());
-
-           
+            yield return null;
         }
     }
 
@@ -148,7 +229,7 @@ public class ConfigJoint : MonoBehaviour
         float heldDownTime = 0;
         while (Input.GetMouseButton(0))
         {
-            if(grappleHasMax && maxJointTime <= heldDownTime)
+            if (grappleHasMax && maxJointTime <= heldDownTime)
             {
                 StopGrapple();
                 StopCoroutine(JointDestroyDelay());
@@ -157,7 +238,7 @@ public class ConfigJoint : MonoBehaviour
             yield return new WaitForSeconds(0);
         }
 
-        if(heldDownTime < minJointTime)
+        if (heldDownTime < minJointTime)
         {
             yield return new WaitForSeconds(minJointTime - heldDownTime);
         }
@@ -170,10 +251,13 @@ public class ConfigJoint : MonoBehaviour
     /// </summary>
     void StopGrapple()
     {
+        isGrappling = false;
+        currentGrappleTarget = null;
+
         lr.positionCount = 0;
         Destroy(joint);
-        objectFixedTo = null;
     }
+
 
 
     void DrawRope()
@@ -181,28 +265,19 @@ public class ConfigJoint : MonoBehaviour
         //If not grappling, don't draw rope
         if (!joint) return;
 
-        currentGrapplePosition = Vector3.Lerp(currentGrapplePosition, grapplePoint, Time.deltaTime * 8f);
+        if (lr.positionCount == 0) return;
 
-        if(lr.positionCount > 0)
-        {
-            lr.SetPosition(0, gunTip.position);
-            lr.SetPosition(1, currentGrapplePosition);
-        }
+        currentGrapplePosition = grapplePoint;
+
+        lr.SetPosition(0, gunTip.position);
+        lr.SetPosition(1, currentGrapplePosition);
     }
 
-    /// <summary>
-    /// Returns true if the player is grappling, false if they are not.
-    /// </summary>
-    /// <returns></returns>
     public bool IsGrappling()
     {
         return joint != null;
     }
 
-    /// <summary>
-    /// Returns the world space point that the player grapples to. 
-    /// </summary>
-    /// <returns></returns>
     public Vector3 GetGrapplePoint()
     {
         return grapplePoint;
@@ -214,7 +289,7 @@ public class ConfigJoint : MonoBehaviour
     /// <returns></returns>
     public float GetMaxGrappleDistance()
     {
-        return maxDistance;
+        return maxPushDistance;
     }
 
     /// <summary>
--- LaunchpadMacaques_Capstone/Assets/Scripts/Player/TempConfigJoint.cs
+++ LaunchpadMacaques_Capstone/Assets/Scripts/Player/TempConfigJoint.cs
@@ -9,18 +9,9 @@ public class TempConfigJoint : MonoBehaviour
     public LayerMask whatIsGrappleable;
     private Vector3 currentGrapplePosition;
     public Transform gunTip, camera, player;
-    private float maxPullDistance = 100f;
-    private float maxPushDistance = 20f;
+    private float maxDistance = 100f;
     private ConfigurableJoint joint;
 
-
-    private enum GrappleType { Pull, Push }
-    //If the player is Pulling, this is the object they're pulling toward
-    private Transform currentGrappleTarget = null;
-    //Offset to account for the grapple target moving while pulling to it
-    private Vector3 currentGrappleTargetOffset = Vector3.zero;
-    //Whether the player is currently Grappling
-    private bool isGrappling = false;
 
     [Header("Explosion Settings")]
     public float explosionRadius = 5f;
@@ -34,61 +25,53 @@ public class TempConfigJoint : MonoBehaviour
     SoftJointLimitSpring springLimit;
     JointDrive jointDrive;
 
-    [Header("Pull - Spring Settings")]
-    [SerializeField] float pullSpringForce = 350;
-    [SerializeField] float pullSpringDamper = 300;
+    [Header("Spring Settings")]
+    [SerializeField] float positionSpringForce = 350;
+    [SerializeField] float positionSpringDamper = 300;
 
-    [Header("Push - Spring Settings")]
-    [SerializeField] float pushSpringForce = 350;
-    [SerializeField] float pushSpringDamper = 300;
+    private GameObject objectFixedTo;
+    private RaycastHit grappleRayHit;
+
+    [Header("Grapple Settings")]
+    [Tooltip("The Min amount of time a joint has to connected before it can be discontented")]
+    [SerializeField] float minJointTime = .08f;
+    [SerializeField] bool grappleHasMax = false;
+
+
+    [Tooltip("The Max amount of time a joint will be connected for")]
+    [SerializeField] float maxJointTime = 2;
 
     void Awake()
     {
         lr = GetComponent<LineRenderer>();
     }
 
-    /// <summary>
-    /// Adjust 
-    /// </summary>
-    /// <param name="grappleType"></param>
-    private void SetSpringSettings(GrappleType grappleType)
+    private void Start()
+    {
+        SetSpringSettings();
+    }
+
+    private void SetSpringSettings()
     {
         jointLimit = new SoftJointLimit();
         springLimit = new SoftJointLimitSpring();
         jointDrive = new JointDrive();
         jointDrive.maximumForce = 3.402823e+38f;
-
-        if (grappleType == GrappleType.Pull)
-        {
-            jointDrive.positionSpring = pullSpringForce;
-            jointDrive.positionDamper = pullSpringDamper;
-        }
-        else
-        {
-            jointDrive.positionSpring = pushSpringForce;
-            jointDrive.positionDamper = pushSpringDamper;
-        }
+        jointDrive.positionSpring = positionSpringForce;
+        jointDrive.positionDamper = positionSpringDamper;
+        lr.positionCount = 0;
     }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && !isGrappling)
+        if (Input.GetMouseButtonDown(0) && joint == null)
         {
-
-            StartGrapple(GrappleType.Pull);
-        }
-        else if (Input.GetMouseButtonUp(0))
-        {
-            StopGrapple();
+            StartGrapple();
         }
 
-        if (Input.GetMouseButton(1) && !isGrappling)
+        if (Input.GetMouseButtonDown(1))
         {
-            StartGrapple(GrappleType.Push);
-        }
-        else if (Input.GetMouseButtonUp(1))
-        {
-            StopGrapple();
+            Explode();
         }
     }
 
@@ -103,7 +86,7 @@ public class TempConfigJoint : MonoBehaviour
     {
         print("Explode");
         RaycastHit hit;
-        if (Physics.Raycast(camera.position, camera.forward, out hit, maxPullDistance, whatIsGrappleable))
+        if (Physics.Raycast(camera.position, camera.forward, out hit, maxDistance, whatIsGrappleable))
         {
             Vector3 explosionPos = transform.position;
             Collider[] colliders = Physics.OverlapSphere(explosionPos, explosionRadius);
@@ -116,6 +99,7 @@ public class TempConfigJoint : MonoBehaviour
                     rb.AddExplosionForce(explosionPower, explosionPos, explosionRadius, 0.0f, ForceMode.Impulse);
                 }
             }
+
         }
     }
 
@@ -123,87 +107,62 @@ public class TempConfigJoint : MonoBehaviour
     /// <summary>
     /// Call whenever we want to start a grapple
     /// </summary>
-    void StartGrapple(GrappleType grappleType)
+    void StartGrapple()
     {
-
         RaycastHit hit;
-        //If pushing and there is a surface in front of the player for them to push off of
-        if (grappleType == GrappleType.Push && Physics.Raycast(camera.position, camera.forward, out hit, maxPushDistance, ~LayerMask.GetMask("CantPush")))
+        if (Physics.Raycast(camera.position, camera.forward, out hit, maxDistance, whatIsGrappleable))
         {
-            isGrappling = true;
+            grappleRayHit = hit;
 
-            lr.positionCount = 0;
-        }
-        //If pulling and there is a surface in front of the player in which they can grapple to
-        else if (grappleType == GrappleType.Pull && Physics.Raycast(camera.position, camera.forward, out hit, maxPullDistance, whatIsGrappleable))
-        {
-            isGrappling = true;
-
-            //Set Grapple target and mark point to pull to
-            currentGrappleTarget = hit.collider.transform;
+            objectFixedTo = hit.collider.gameObject;
             grapplePoint = hit.point;
-            currentGrappleTargetOffset = grapplePoint - currentGrappleTarget.position;
-
-            lr.positionCount = 2;
-            currentGrapplePosition = gunTip.position;
-        }
-
-        //Not able to pull or push
-        if (!isGrappling)
-            return;
-
-        //Joint setup based on push or pull
-        joint = player.gameObject.AddComponent<ConfigurableJoint>();
-        joint.autoConfigureConnectedAnchor = false;
-
-        SetSpringSettings(grappleType);
-        joint.xDrive = jointDrive;
-        joint.yDrive = jointDrive;
-        joint.zDrive = jointDrive;
-
-        StartCoroutine(UpdateGrapplePosition(grappleType));
-    }
-
-    /// <summary>
-    /// Coroutine that updates the position of the grapple anchor to the appropriate point and then updates the joint
-    /// </summary>
-    /// <param name="grappleType">Push or Pull</param>
-    /// <returns></returns>
-    IEnumerator UpdateGrapplePosition(GrappleType grappleType)
-    {
-        RaycastHit hit;
-
-        while (isGrappling)
-        {
-            //Pushing
-            if (grappleType == GrappleType.Push && Physics.Raycast(camera.position, camera.forward, out hit, maxPushDistance, ~LayerMask.GetMask("CantPush")))
-            {
-                //Update grapple point to arbitrary point behind player
-                grapplePoint = hit.point + -(camera.forward * maxPushDistance);
-            }
-            else if (grappleType == GrappleType.Push)
-            {
-                StopGrapple();
-            }
-            //Pulling
-            else if (grappleType == GrappleType.Pull)
-            {
-                //Update grapple point to the target + original offset
-                grapplePoint = currentGrappleTarget.position + currentGrappleTargetOffset;
-            }
-
-
-            //Update Joint
+            joint = player.gameObject.AddComponent<ConfigurableJoint>();
+            joint.autoConfigureConnectedAnchor = false;
             joint.connectedAnchor = grapplePoint;
 
             float distanceFromPoint = Vector3.Distance(player.position, grapplePoint);
             jointLimit.limit = distanceFromPoint;
             joint.targetPosition = Vector3.zero;
 
+
+            joint.xDrive = jointDrive;
+            joint.yDrive = jointDrive;
+            joint.zDrive = jointDrive;
+
+
+            lr.positionCount = 2;
             currentGrapplePosition = gunTip.position;
 
-            yield return null;
+            StartCoroutine(JointDestroyDelay());
+
+
         }
+    }
+
+    /// <summary>
+    /// Will Handle Forcing the player to have the joint enabled for a min set of time
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator JointDestroyDelay()
+    {
+        float heldDownTime = 0;
+        while (Input.GetMouseButton(0))
+        {
+            if (grappleHasMax && maxJointTime <= heldDownTime)
+            {
+                StopGrapple();
+                StopCoroutine(JointDestroyDelay());
+            }
+            heldDownTime += Time.deltaTime;
+            yield return new WaitForSeconds(0);
+        }
+
+        if (heldDownTime < minJointTime)
+        {
+            yield return new WaitForSeconds(minJointTime - heldDownTime);
+        }
+
+        StopGrapple();
     }
 
     /// <summary>
@@ -211,13 +170,10 @@ public class TempConfigJoint : MonoBehaviour
     /// </summary>
     void StopGrapple()
     {
-        isGrappling = false;
-        currentGrappleTarget = null;
-
         lr.positionCount = 0;
         Destroy(joint);
+        objectFixedTo = null;
     }
-
 
 
     void DrawRope()
@@ -225,21 +181,48 @@ public class TempConfigJoint : MonoBehaviour
         //If not grappling, don't draw rope
         if (!joint) return;
 
-        if (lr.positionCount == 0) return;
+        currentGrapplePosition = Vector3.Lerp(currentGrapplePosition, grapplePoint, Time.deltaTime * 8f);
 
-        currentGrapplePosition = grapplePoint;
-
-        lr.SetPosition(0, gunTip.position);
-        lr.SetPosition(1, currentGrapplePosition);
+        if (lr.positionCount > 0)
+        {
+            lr.SetPosition(0, gunTip.position);
+            lr.SetPosition(1, currentGrapplePosition);
+        }
     }
 
+    /// <summary>
+    /// Returns true if the player is grappling, false if they are not.
+    /// </summary>
+    /// <returns></returns>
     public bool IsGrappling()
     {
         return joint != null;
     }
 
+    /// <summary>
+    /// Returns the world space point that the player grapples to. 
+    /// </summary>
+    /// <returns></returns>
     public Vector3 GetGrapplePoint()
     {
         return grapplePoint;
+    }
+
+    /// <summary>
+    /// Returns the max grapple distance possible. 
+    /// </summary>
+    /// <returns></returns>
+    public float GetMaxGrappleDistance()
+    {
+        return maxDistance;
+    }
+
+    /// <summary>
+    /// Returns the Rayhit from the grappling Raycast. 
+    /// </summary>
+    /// <returns></returns>
+    public RaycastHit GetGrappleRayhit()
+    {
+        return grappleRayHit;
     }
 }
