9bdce2eedb3700371268acaa7b071ae4f69bd4c1
CJ Green <55067215+Godofwar1300@users.noreply.github.com>
2020-09-17 12:24:25 -0500

    Updated the colleectible script and added a collectible manager.
    Should be added to both scenes.
    
    Added a folder for organization

---
+++ LaunchpadMacaques_Capstone/Assets/Scripts/Collectibles/CollectibleController.cs
@@ -0,0 +1,34 @@
+﻿/* 
+* Launchpad Macaques 
+* CJ Green
+* CollectibleController.cs 
+* Handles the UI when collecting stuff.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CollectibleController : MonoBehaviour
+{
+    public int totalCollectibles;
+    public TextMeshProUGUI totalCollectiblesText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalCollectiblesText.SetText("Total Collectibles: 0");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int GetTotalCollectibles()
+    {
+        return totalCollectibles;
+    }
+}
---
+++ LaunchpadMacaques_Capstone/Assets/Scripts/Collectibles/CollectibleSphereScript.cs
@@ -0,0 +1,50 @@
+﻿/* 
+* Launchpad Macaques 
+* Colin Bugbee
+* CJ Green
+* CollectibleSphereScript.cs 
+* Destroys collectibles upon player collision.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityStandardAssets._2D;
+
+public class CollectibleSphereScript : MonoBehaviour
+{
+    [SerializeField]
+    private CollectibleController collectibleController;
+    private int plusOne = 1;
+
+    private void Awake()
+    {
+        collectibleController = GameObject.Find("Collectible Controller").GetComponent<CollectibleController>();
+    }
+
+    private void Collect()
+    {
+        AddToTotalCollectibles(plusOne);
+        Destroy(this.gameObject);
+    }
+
+    public void AddToTotalCollectibles(int plusOne)
+    {
+        int totalCollectibles = 0;
+
+        totalCollectibles= collectibleController.totalCollectibles = collectibleController.GetTotalCollectibles() + plusOne;
+
+        collectibleController.totalCollectiblesText.SetText("Total Collectibles: " + totalCollectibles);
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Collect();
+        }
+    }
+}
