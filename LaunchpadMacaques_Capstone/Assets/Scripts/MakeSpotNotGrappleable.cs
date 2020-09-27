ef4e0afc84ebd862b05e326af0f7a1698a0a37e0
Levi Schoof <levischoof.99@gmail.com>
2020-09-27 14:25:57 -0500

    A few small changes and Commented Code
    abililty to make entier object not grappable, fixed bugs, and created new material

--- LaunchpadMacaques_Capstone/Assets/Scripts/MakeSpotNotGrappleable.cs
+++ LaunchpadMacaques_Capstone/Assets/Scripts/MakeSpotNotGrappleable.cs
@@ -5,45 +5,75 @@ using UnityEngine.Rendering.HighDefinition;
 
 public class MakeSpotNotGrappleable : MonoBehaviour
 {
-    [SerializeField] float notGrappableSize = 5;
-    [SerializeField] GameObject corruptedVisual;
+    #region Public Values
+    [Header("Size Settings")]
+    [SerializeField][Tooltip("The Size of How much of the object will be made not Grappable")] float notGrappableSize = 5;
+    [SerializeField][Tooltip("The Max Size of an object for the entire object to become un Grappable")] float maxObjectSize = 13;
+    [Header("Visuals")]
+    [SerializeField][Tooltip("The Object that is spawned to make part of object not Grappable")] GameObject corruptedVisual;
+    [SerializeField][Tooltip("The Material an object will switch to if the entire object becomes un Grappable")] Material corruptedMaterial;
 
-    [Tooltip("The Prefab to be used as an aiming decal.")] [SerializeField] private GameObject decal;
-    [Tooltip("The layers the aiming decal should be enabled on.")] [SerializeField] private LayerMask whatIsGrappleable;
+    [SerializeField] [Tooltip("The Prefab that creates the Decal that is placed on part of Object when it becomes un Grappable")] private GameObject decal;
+    [SerializeField] private LayerMask whatIsGrappleable;
+    #endregion
 
+    #region Private Values
     private GameObject grappleDecalObj;
+    private float objectSize;
 
 
     private Matt_PlayerMovement player;
     private Camera cam;
+    #endregion
 
-    //[SerializeField] Material grappleMaterial;
-    // Start is called before the first frame update
+
+
     void Start()
+    {
+        SetUp();
+    }
+
+
+    /// <summary>
+    /// Will find and set the needed objects
+    /// </summary>
+    private void SetUp()
     {
         cam = FindObjectOfType<Camera>();
         player = FindObjectOfType<Matt_PlayerMovement>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
 
-    }
-
-    //public void MakeSpotNotGrappable(Vector3 spotPos, GameObject hitObject)
-    //{
-    //    Debug.Log("Corrupted");
-    //    GameObject temporaryCorruptedVisual =  Instantiate(corruptedVisual);
-    //    temporaryCorruptedVisual.transform.LookAt(player.transform.position + cam.transform.rotation * Vector3.back, cam.transform.rotation * Vector3.up);
-    //    temporaryCorruptedVisual.transform.position = spotPos;
-    //    temporaryCorruptedVisual.transform.localScale = new Vector3(notGrappableSize, notGrappableSize, notGrappableSize);
-    //    temporaryCorruptedVisual.transform.parent = hitObject.transform;
-    //}
-
+    /// <summary>
+    /// What is Called to Determine how an object/spot is made un Grappable
+    /// </summary>
+    /// <param name="spotPos"></param>
+    /// <param name="hitObject"></param>
     public void MakeSpotNotGrappable(RaycastHit spotPos, GameObject hitObject)
     {
+        Vector3 objectVector = hitObject.GetComponent<Renderer>().bounds.size;
+        objectSize = objectVector.x + objectVector.y + objectVector.z;
 
+        Debug.Log("Object Size: " + objectSize);
+
+        if(objectSize > maxObjectSize)
+        {
+            MakePartOfObjectNotGrappable(spotPos, hitObject);
+        }
+
+        else
+        {
+            MakeEntireObjectNotGrappable(hitObject);
+        }
+    }
+
+    /// <summary>
+    /// Will Make part of an object un Grappable and will place a decal there
+    /// </summary>
+    /// <param name="spotPos"></param>
+    /// <param name="hitObject"></param>
+    private void MakePartOfObjectNotGrappable(RaycastHit spotPos, GameObject hitObject)
+    {
         GameObject temporaryCorruptedVisual = Instantiate(corruptedVisual);
         temporaryCorruptedVisual.transform.LookAt(player.transform.position + cam.transform.rotation * Vector3.back, cam.transform.rotation * Vector3.up);
         temporaryCorruptedVisual.transform.position = spotPos.point;
@@ -58,18 +88,27 @@ public class MakeSpotNotGrappleable : MonoBehaviour
         grappleDecalObj.transform.rotation = Quaternion.FromToRotation(new Vector3(Vector3.up.x, Vector3.up.y, Vector3.up.z + 90), spotPos.normal);
         grappleDecalObj.GetComponent<DecalProjector>().size = new Vector3(notGrappableSize * 1.25f, notGrappableSize * 1.25f, notGrappableSize * 1.25f);
         grappleDecalObj.transform.parent = hitObject.transform;
+    }
 
-
-
-        //Material mymat = grappleDecalObj.GetComponent<DecalProjector>().material = new Material((grappleDecalObj.GetComponent<DecalProjector>().material.shader));
-        //Material mymat = grappleDecalObj.GetComponent<DecalProjector>().material;
-        //mymat.SetColor("_EmissiveColor", Color.red);
+    /// <summary>
+    /// Will Make an entire object un Grappable and will change its material
+    /// </summary>
+    /// <param name="hitObject"></param>
+    private void MakeEntireObjectNotGrappable(GameObject hitObject)
+    {
+        StartCoroutine(ChangeObjectLayer(hitObject));
+        hitObject.GetComponent<Renderer>().material = corruptedMaterial;
     }
 
 
-    private void MoveDecal(RaycastHit info)
+    /// <summary>
+    /// Will Change the Layer of the Object to "CantGrapple" After short ammount of time
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns></returns>
+    IEnumerator ChangeObjectLayer(GameObject obj)
     {
-        grappleDecalObj.transform.position = info.point;
-        grappleDecalObj.transform.rotation = Quaternion.FromToRotation(new Vector3(Vector3.up.x, Vector3.up.y, Vector3.up.z + 90), info.normal);
+        yield return new WaitForSeconds(.1f);
+        obj.layer = LayerMask.NameToLayer("CantGrapple");
     }
 }
