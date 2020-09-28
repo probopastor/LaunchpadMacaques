f9821657943b68608a60ad8a78e18199571ce8de
Levi Schoof <levischoof.99@gmail.com>
2020-09-28 14:11:37 -0500

    Rough Version of Grabbing and Pushing Objects

---
+++ LaunchpadMacaques_Capstone/Assets/Scripts/Push Pull Objects/PushPullObjects.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushPullObjects : MonoBehaviour
+{
+    [SerializeField] float maxGrabDistance;
+    [SerializeField] GameObject objectHolder;
+    [SerializeField] LayerMask canBePickedUp;
+    [SerializeField] float objectFollowSpeed = 10;
+    [SerializeField] GameObject hand;
+    [SerializeField] float throwForce = 10;
+
+
+    private Rigidbody objectRB;
+    private GameObject cam;
+    private bool grabbing = false;
+    private LineRenderer lr;
+    //// Start is called before the first frame update
+    void Start()
+    {
+        cam = FindObjectOfType<Camera>().gameObject;
+        lr = this.GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (grabbing)
+            {
+                DropObject();
+            }
+
+            else
+            {
+                PickUpObject();
+            }
+  
+        }
+
+        if(Input.GetMouseButtonDown(1) & grabbing)
+        {
+           ThrowObject();
+        }
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (grabbing)
+        {
+            objectRB.MovePosition(Vector3.Lerp(objectRB.position, objectHolder.transform.position, Time.deltaTime * objectFollowSpeed));
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (grabbing)
+        {
+            DrawRope();
+        }
+
+    }
+
+    private void PickUpObject()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, maxGrabDistance, canBePickedUp))
+        {
+            objectRB = hit.rigidbody;
+            objectRB.isKinematic = true;
+
+            grabbing = true;
+
+            lr.positionCount = 2;
+            objectRB.GetComponent<PushableObj>().PickedUpObject(throwForce, cam);
+
+
+        }
+    }
+
+    private void DropObject()
+    {
+        grabbing = false;
+        objectRB.isKinematic = false;
+        objectRB.GetComponent<PushableObj>().DroppedObject();
+        objectRB = null;
+        lr.positionCount = 0;
+
+    }
+
+    private void ThrowObject()
+    {
+        objectRB.isKinematic = false;
+        objectRB.GetComponent<PushableObj>().StartPush(throwForce, cam);
+        objectRB.useGravity = false;
+        grabbing = false;
+        lr.positionCount = 0;
+        objectRB = null;
+
+    }
+
+
+    void DrawRope()
+    { 
+        lr.SetPosition(0, objectRB.transform.position);
+        lr.SetPosition(1, hand.transform.position);
+    }
+
+}
---
+++ LaunchpadMacaques_Capstone/Assets/Scripts/Push Pull Objects/PushableObj.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using UnityEngine;
+
+public class PushableObj : MonoBehaviour
+{
+    private bool beingPushed;
+    private bool pickedUp = false;
+
+    [SerializeField] GameObject throwDecal;
+    private GameObject thisDecal;
+    [SerializeField] Vector3 objectVelocity;
+    [SerializeField] int predStepsPerFrame;
+
+    private float tempSpeed;
+    private GameObject tempCam;
+
+    private LineRenderer lr;
+    private void Start()
+    {
+        thisDecal = Instantiate(throwDecal);
+        thisDecal.SetActive(false);
+        beingPushed = false;
+        lr = this.GetComponent<LineRenderer>();
+        lr.positionCount = 0;
+    }
+    public void StartPush(float speed, GameObject cam)
+    {
+        beingPushed = true;
+        objectVelocity = speed * cam.transform.forward;
+    }
+
+    private void Update()
+    {
+        if (beingPushed) PushObject();
+
+        else if (pickedUp) ShowLine();
+    }
+
+    private void PushObject()
+    {
+        pickedUp = false;
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+        Vector3 point1 = this.transform.position;
+        float stepSize = 1.0f / predStepsPerFrame;
+
+
+        for (float step = 0; step < 1; step += stepSize)
+        {
+            objectVelocity += Physics.gravity * stepSize * Time.deltaTime;
+            Vector3 point2 = point1 + objectVelocity * stepSize * Time.deltaTime;
+
+            Ray ray = new Ray(point1, point2 - point1);
+            if (Physics.Raycast(ray, (point2 - point1).magnitude))
+            {
+                StopPushingObject();
+            }
+
+
+            point1 = point2;
+
+
+        }
+
+        this.transform.position = point1;
+    }
+
+    private void ShowLine()
+    {
+        Vector3 point1 = this.transform.position;
+        Vector3 predObjectVelocity = objectVelocity;
+        predObjectVelocity = tempSpeed * tempCam.transform.forward;
+        float stepSize = .1f;
+        lr.positionCount = 2;
+        lr.SetPosition(0, this.transform.position);
+        int count = 1;
+        for (float step = 0; step < 500; step += stepSize)
+        {
+            predObjectVelocity += Physics.gravity * stepSize;
+            Vector3 point2 = point1 + predObjectVelocity * stepSize;
+
+            RaycastHit hit;
+            Ray ray = new Ray(point1, point2 - point1);
+            if (Physics.Raycast(ray, out hit, (point2 - point1).magnitude))
+            {
+                lr.positionCount = count;
+                thisDecal.SetActive(true);
+                MoveDecal(hit);
+                break;
+            }
+
+            lr.SetPosition(count, point2);
+            point1 = point2;
+            count++;
+            lr.positionCount++;
+
+
+        }
+
+    }
+
+    private void MoveDecal(RaycastHit info)
+    {
+        thisDecal.transform.position = info.point;
+        thisDecal.transform.rotation = Quaternion.FromToRotation(new Vector3(Vector3.up.x, Vector3.up.y, Vector3.up.z + 90), info.normal);
+
+    }
+
+
+    public void PickedUpObject(float speed, GameObject cam)
+    {
+        tempSpeed = speed;
+        tempCam = cam;
+        pickedUp = true;
+
+    }
+
+    public void DroppedObject()
+    {
+        pickedUp = false;
+        beingPushed = false;
+        GetComponent<Rigidbody>().useGravity = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        StopPushingObject();
+    }
+
+    private void StopPushingObject()
+    {
+        beingPushed = false;
+        GetComponent<Rigidbody>().useGravity = true;
+    }
+}
