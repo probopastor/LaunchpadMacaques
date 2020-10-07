3cc1987f2d190910439679243f07eef7b80ea9d3
probopastor <43503592+probopastor@users.noreply.github.com>
2020-10-07 08:05:15 -0500

    Made PassTriggerEnabler.cs variables have tooltips
    -Gave tooltips to variables
    -Made public variablea [SerializeField] private

--- LaunchpadMacaques_Capstone/Assets/Scripts/PassTriggerEnabler.cs
+++ LaunchpadMacaques_Capstone/Assets/Scripts/PassTriggerEnabler.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class PassTriggerEnabler : MonoBehaviour
 {
-    public GameObject[] targets;
+    [SerializeField, Tooltip("An array of targets to be altered by the Pass-By Trigger. ")] private GameObject[] targets;
     public bool activated = false;
     public bool targetEnabled = false;
-    public bool timer = false;
-    public int timerValue = 5;
+    [SerializeField, Tooltip("If enabled, disables all targets after a period of time. ")] private bool timer = false;
+    [SerializeField, Tooltip("Time before targets are disabled after button click (Only if useTimer is used). ")] private int timerValue = 5;
     private int triggers = 0;
-    public int triggerEnableGoal = 2;
-    public int triggerDisableGoal = 1;
-    public Material active;
-    public Material inactive;
+    [SerializeField, Tooltip("The amount of triggers required for objects to be enabled. ")] private int triggerEnableGoal = 2;
+    [SerializeField, Tooltip("The amount of triggers required for objects to be disabled. ")] private int triggerDisableGoal = 1;
+    [SerializeField, Tooltip("Material of active object. ")] private Material active;
+    [SerializeField, Tooltip("Material of inactive object. ")] private Material inactive;
 
     // Start is called before the first frame update
     void Start()
