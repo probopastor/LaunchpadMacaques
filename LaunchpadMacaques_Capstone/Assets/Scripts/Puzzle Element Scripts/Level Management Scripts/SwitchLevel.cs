28aef0aa2b1d0fad6403261bd16631a6d2199365
probopastor <43503592+probopastor@users.noreply.github.com>
2020-10-10 10:05:41 -0500

    Made a Game Object that moves you to the next level upon level completion
    - Put in levels 1 and 3

---
+++ LaunchpadMacaques_Capstone/Assets/Scripts/Puzzle Element Scripts/Level Management Scripts/SwitchLevel.cs
@@ -0,0 +1,40 @@
+﻿/* 
+* Launchpad Macaques 
+* William Nomikos
+* SwitchLevel.cs 
+* Handles basic scene and level switching when triggering a set object. 
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchLevel : MonoBehaviour
+{
+    [SerializeField, Tooltip("The name of the scene this object should load when triggered. ")] private string nextLevelName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SwitchScenes();
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            SwitchScenes();
+        }
+    }
+
+    /// <summary>
+    /// Switches the scene the player is in to a designated scene. 
+    /// </summary>
+    private void SwitchScenes()
+    {
+        SceneManager.LoadScene(nextLevelName);
+    }
+}
