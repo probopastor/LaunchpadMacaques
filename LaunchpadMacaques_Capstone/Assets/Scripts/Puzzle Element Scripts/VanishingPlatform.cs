fcb6930bab730111c84d47d6985795a3fa866667
CJ Green <55067215+Godofwar1300@users.noreply.github.com>
2020-10-27 20:00:28 -0500

    Pushing Stuff

---
+++ LaunchpadMacaques_Capstone/Assets/Scripts/Puzzle Element Scripts/VanishingPlatform.cs
@@ -0,0 +1,77 @@
+﻿/* 
+* (Launchpad Macaques - [Game Name Here]) 
+* (Contributors/Author(s)) 
+* (File Name) 
+* (Describe, in general, the code contained.) 
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VanishingPlatform : MonoBehaviour
+{
+    [SerializeField, Tooltip("This is the timer for how long the timer will dissappear and reappear for.")]
+    private float timerValue = 2.5f;
+
+    [SerializeField, Tooltip("This is whether the platform is visible or not.")]
+    private bool isVisible = true;
+
+    [SerializeField, Tooltip("This is an array of tag names that determine the variation of ")]
+    private string[] tagNames = { "Platform 1", "Platform 2" };
+
+    private GrapplingGun grappleGun;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        grappleGun = FindObjectOfType<GrapplingGun>().GetComponent<GrapplingGun>();
+
+
+        if (gameObject.tag == tagNames[0])
+        {
+            isVisible = true;
+            StartCoroutine(Vanish());
+        }
+        else if (gameObject.tag == tagNames[1])
+        {
+            isVisible = false;
+            StartCoroutine(Vanish());
+        }
+    }
+
+    IEnumerator Vanish()
+    {
+        if (isVisible)
+        {
+            Debug.Log("Dissappearing...");
+
+            grappleGun.StopGrapple();
+
+            gameObject.GetComponent<MeshRenderer>().enabled = false;
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+
+            yield return new WaitForSecondsRealtime(timerValue);
+
+            isVisible = false;
+
+            StartCoroutine(Vanish());
+        }
+
+        if (!isVisible)
+        {
+            Debug.Log("Reappearing!!");
+
+            gameObject.GetComponent<MeshRenderer>().enabled = true;
+            gameObject.GetComponent<BoxCollider>().enabled = true;
+
+            yield return new WaitForSecondsRealtime(timerValue);
+
+            isVisible = true;
+
+            StartCoroutine(Vanish());
+        }
+    }
+
+}
