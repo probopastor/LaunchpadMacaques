99fbdb706387426956bb34ee6c7bfa982ad110fd
Anthony <awessel@mail.bradley.edu>
2020-11-05 09:34:36 -0600

    Added current corruption decal
    uses LockToWorldGrid.cs to adjust its position so that the lines match up

---
+++ LaunchpadMacaques_Capstone/Assets/Scripts/LockToWorldGrid.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockToWorldGrid : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x -= pos.x % 0.625f;
+        pos.y -= pos.y % 0.625f;
+        pos.z -= pos.z % 0.625f;
+
+        transform.position = pos;
+    }
+}
