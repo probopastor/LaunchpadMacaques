3ef9c90ae07bbc3045cfaad92145db158bc5b02e
Levi Schoof <levischoof.99@gmail.com>
2020-11-15 12:00:34 -0600

    Added comments and reformatted the Foot Step script

--- LaunchpadMacaques_Capstone/Assets/Scripts/Player/Grappling Scripts/FootStepCorruption.cs
+++ LaunchpadMacaques_Capstone/Assets/Scripts/Player/Grappling Scripts/FootStepCorruption.cs
@@ -1,33 +1,34 @@
-﻿using System.Collections;
+﻿/* 
+* (Launchpad Macaques - [Trial and Error]) 
+* (Levi Schoof) 
+* (FootStepCorruption.cs) 
+* (Will handle the creation of footstep corruption) 
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.HighDefinition;
 
 public class FootStepCorruption : MonoBehaviour
 {
-    [SerializeField] LayerMask ground;
-    [SerializeField] GameObject footDecal;
-    [SerializeField] float decalSize;
-    [SerializeField] float timeBetweenSteps;
-    [SerializeField] bool scaleWithPlayerSpeed = true;
-    [SerializeField] float scaleAmount = .1f;
-    [SerializeField] int maxFootSteps;
+    [SerializeField, Tooltip("The Layer that the Footsteps can be placed")] LayerMask ground;
+    [SerializeField, Tooltip("The Decal Projector that is used for the Foot prints")] GameObject footDecal;
+    [SerializeField, Tooltip("The size of the Foot Prints")] float decalSize;
+    [SerializeField, Tooltip("The base time between foot prints, while the player is moving")] float timeBetweenSteps;
+  //  [SerializeField, Tooltip("If true, the time between foot prints will be reduced based on the players speed")] bool scaleWithPlayerSpeed = true;
+    [SerializeField, Tooltip("The ammount that is multiplied by the players velocity to scale timeBetweenSteps")] float scaleAmount = .1f;
+    [SerializeField, Tooltip("The max ammount of Foot steps that can be spawned, will remove the oldest ones")] int maxFootSteps;
 
-    [SerializeField] GameObject leftFootPos;
-    [SerializeField] GameObject rightFootPos;
+    [SerializeField, Tooltip("The position where a raycast will be drawn down to determine where left foot print should go")] GameObject leftFootPos;
+    [SerializeField, Tooltip("The position where a raycast will be drawn down to determine where right foot print should go")] GameObject rightFootPos;
 
     private bool rightFoot;
-
     private Matt_PlayerMovement player;
     private Rigidbody playerRB;
-
-    private bool inCourtine = false;
-
     private MakeSpotNotGrappleable coruptedTracker;
     private GameObject decal;
-
     RaycastHit spotPos;
-
     List<GameObject> decals;
     // Start is called before the first frame update
     void Start()
@@ -38,94 +39,114 @@ public class FootStepCorruption : MonoBehaviour
 
         decals = new List<GameObject>();
         rightFoot = true;
+
+        StartCoroutine(FootSteps());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(!inCourtine && playerRB.velocity.magnitude != 0)
-        {
-        
-            StartCoroutine(FootSteps());
-        }
-
-        //else if(playerRB.velocity.magnitude == 0)
-        //{
-        //    StopCoroutine(FootSteps());
-        //    inCourtine = false;
-        //}
-    }
-
+    /// <summary>
+    /// Will Handle the creation of the FootSteps
+    /// </summary>
+    /// <returns></returns>
     IEnumerator FootSteps()
     {
-        inCourtine = true;
         while (true)
         {
-            Transform tempTrans;
+            CreateDecal(DetermineWhichFoot());
+            ManageDecalList();
+            yield return new WaitForSeconds(DetermineWaitTime());
+        }
+    }
 
-            if (rightFoot)
+    /// <summary>
+    /// Returns which foot should be used
+    /// </summary>
+    /// <returns></returns>
+    private Transform DetermineWhichFoot()
+    {
+
+        if (rightFoot)
+        {
+            rightFoot = false;
+            return rightFootPos.transform;
+
+        }
+
+        else
+        {
+            rightFoot = true;
+            return leftFootPos.transform;
+
+        }
+
+    }
+
+    /// <summary>
+    /// Will make sure there are not to many foot step decals, if there is remove the decal
+    /// </summary>
+    private void ManageDecalList()
+    {
+        if (decals.Count >= maxFootSteps)
+        {
+            coruptedTracker.RemoveCorruptedDecal(decals[0]);
+            GameObject temp = decals[0];
+
+            decals.RemoveAt(0);
+            Destroy(temp.gameObject);
+        }
+    }
+
+    /// <summary>
+    /// Will return how long the routine should wait based on how fas the player is moving
+    /// </summary>
+    /// <returns></returns>
+    private float DetermineWaitTime()
+    {
+        if (playerRB.velocity.magnitude > 5)
+        {
+            return timeBetweenSteps * (1 / (playerRB.velocity.magnitude * scaleAmount));
+        }
+
+        else
+        {
+            return 0;
+        }
+    }
+
+    /// <summary>
+    /// Will create the foot decal as a specific position, and will snap it to the grid
+    /// </summary>
+    /// <param name="tempTrans"></param>
+    private void CreateDecal(Transform tempTrans)
+    {
+        if (Physics.Raycast(tempTrans.position, Vector3.down, out spotPos, 5, ground) && playerRB.velocity.magnitude > 5)
+        {
+            Debug.Log("Made THing");
+            float maxNormal = Mathf.Max(Mathf.Max(Mathf.Abs(spotPos.normal.x), Mathf.Abs(spotPos.normal.y)), Mathf.Abs(spotPos.normal.z));
+
+            Vector3 point = spotPos.point;
+
+            // Lock the decal to a grid, but ignore the axis that is closest to the normal. (to prevent the decal being forced off of the surface)
+            if (Mathf.Abs(spotPos.normal.x) != maxNormal)
             {
-                tempTrans = rightFootPos.transform;
-                rightFoot = false;
+                point.x -= (point.x % 0.625f);
+            }
+            if (Mathf.Abs(spotPos.normal.y) != maxNormal)
+            {
+                point.y -= (point.y % 0.625f);
+            }
+            if (Mathf.Abs(spotPos.normal.z) != maxNormal)
+            {
+                point.z -= (point.z % 0.625f);
             }
 
-            else
-            {
-                tempTrans = leftFootPos.transform;
-                rightFoot = true;
-            }
-            if (Physics.Raycast(tempTrans.position, Vector3.down, out spotPos, 5, ground) && playerRB.velocity.magnitude > 5)
-            {
-                Debug.Log("Made THing");
-                float maxNormal = Mathf.Max(Mathf.Max(Mathf.Abs(spotPos.normal.x), Mathf.Abs(spotPos.normal.y)), Mathf.Abs(spotPos.normal.z));
+            decal = Instantiate(footDecal);
+            decal.transform.position = point;
+            decal.transform.rotation = Quaternion.FromToRotation(new Vector3(Vector3.up.x, Vector3.up.y, Vector3.up.z + 90), spotPos.normal);
+            decal.GetComponent<DecalProjector>().size = new Vector3(decalSize, decalSize, .1f);
+            coruptedTracker.AddCorruptedDecals(decal);
+            decal.transform.parent = spotPos.transform;
 
-                Vector3 point = spotPos.point;
-
-                // Lock the decal to a grid, but ignore the axis that is closest to the normal. (to prevent the decal being forced off of the surface)
-                if (Mathf.Abs(spotPos.normal.x) != maxNormal)
-                {
-                    point.x -= (point.x % 0.625f);
-                }
-                if (Mathf.Abs(spotPos.normal.y) != maxNormal)
-                {
-                    point.y -= (point.y % 0.625f);
-                }
-                if (Mathf.Abs(spotPos.normal.z) != maxNormal)
-                {
-                    point.z -= (point.z % 0.625f);
-                }
-
-                decal = Instantiate(footDecal);
-                decal.transform.position = point;
-                decal.transform.rotation = Quaternion.FromToRotation(new Vector3(Vector3.up.x, Vector3.up.y, Vector3.up.z + 90), spotPos.normal);
-                decal.GetComponent<DecalProjector>().size = new Vector3(decalSize, decalSize, .1f);
-                coruptedTracker.AddCorruptedDecals(decal);
-                decal.transform.parent = spotPos.transform;
-
-                decals.Add(decal);
-            }
-
-
-            if(decals.Count >= maxFootSteps)
-            {
-                coruptedTracker.RemoveCorruptedDecal(decals[0]);
-                GameObject temp = decals[0];
-
-                decals.RemoveAt(0);
-                Destroy(temp.gameObject);
-            }
-
-            if(playerRB.velocity.magnitude > 5)
-            {
-                yield return new WaitForSeconds((timeBetweenSteps * (1 / (playerRB.velocity.magnitude * scaleAmount))));
-            }
-
-            else
-            {
-                yield return new WaitForSeconds(0);
-            }
-    
-
+            decals.Add(decal);
         }
     }
 }
