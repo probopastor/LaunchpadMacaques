1776c8a3b0207da4e3670908a87f218697352e36
Zackary Seiple <zackaryseiple@gmail.com>
2020-12-03 21:26:51 -0600

    Event System Integrated
    -OnPlayerHitGround Event
    -When Player looks at specified objects
    -Trigger a set amount of time after still in level

--- LaunchpadMacaques_Capstone/Assets/Scripts/Tools/NarrativeTriggerHandler.cs
+++ LaunchpadMacaques_Capstone/Assets/Scripts/Tools/NarrativeTriggerHandler.cs
@@ -16,10 +16,17 @@ using UnityEngine.Events;
 using UnityEngine.UI;
 using TMPro;
 
+[RequireComponent(typeof(GameEventManager))]
 public class NarrativeTriggerHandler : MonoBehaviour
 {
     public enum TriggerType { Area, Random, OnEvent };
-    //public enum EventTypes { }
+
+    #region Events
+    public enum EventType { PlayerHitGround , TimeInLevel, LookAtObject}
+
+    UnityAction onPlayerHitGround;
+    #endregion
+
     [SerializeField, Tooltip("Contains all triggers in the scene. Each array element is a separate trigger with it's own type, " +
         "as well as its own text and audio outputs")]
     public Trigger[] triggers;
@@ -33,6 +40,8 @@ public class NarrativeTriggerHandler : MonoBehaviour
     private float randomIntervalMin;
     [SerializeField, Tooltip("The upper end of the interval that a random trigger can be called")]
     private float randomIntervalMax;
+    [SerializeField, Tooltip("The time (in seconds) that the player must be falling in order for the \"Player Hitting Ground\" event to trigger")]
+    private float fallTime;
     [SerializeField]
     private TMP_Text dialogue;
 
@@ -42,7 +51,7 @@ public class NarrativeTriggerHandler : MonoBehaviour
     {
         [Tooltip("The type of activation used for this Trigger")]
         public TriggerType type;
-        [Tooltip("The text to display on Trigger activation")]
+        [SerializeField, Tooltip("The text to display on Trigger activation")]
         public string textToDisplay;
         [Tooltip("The time text should be displayed")]
         public float textDisplayTime;
@@ -53,6 +62,7 @@ public class NarrativeTriggerHandler : MonoBehaviour
         [Tooltip("Whether this Trigger can be activated multiple times (true) or only once (false)")]
         public bool repeatable;
         public bool hasRan = false;
+        public bool isRunning = false;
 
         //Only appear on TriggerType.Area
         [Tooltip("The tag of objects that will activate the Trigger when it enters the trigger area")]
@@ -62,13 +72,32 @@ public class NarrativeTriggerHandler : MonoBehaviour
         public Vector3 areaCenter;
         [Tooltip("The size of the trigger zone relative to the center")]
         public Vector3 boxSize;
-      
+
+        //Only appear on TriggerType.OnEvent
+        [Tooltip("The type of event for this trigger")]
+        public EventType eventType;
+        [Tooltip("The amount of time (in seconds) the player has to be in the level for this to trigger")]
+        public float timeInLevelBeforeTrigger = 0;
+        [Tooltip("The array of objects that, when looked at, will activate this trigger")]
+        public GameObject[] triggeringObjects;
         
+    }
+
+    private void OnEnable()
+    {
+        GameEventManager.StartListening("onPlayerHitGround", onPlayerHitGround);
+        UnityEngine.SceneManagement.SceneManager.activeSceneChanged += TimeInLevelCountStart;
+    }
+
+    private void OnDisable()
+    {
+        GameEventManager.StopListening("onPlayerHitGround", onPlayerHitGround);
     }
 
     private void Awake()
     {
         dialogue = GetComponentInChildren<TMP_Text>();
+        onPlayerHitGround += PlayerHitGroundActivation;
     }
 
     private void Start()
@@ -155,8 +184,31 @@ public class NarrativeTriggerHandler : MonoBehaviour
         //Text appearing functionality
     }
 
+    public void ObjectInSightCheck(GameObject obj)
+    {
+        if (obj == null)
+            return;
+
+        Trigger currentTrigger;
+        for(int i = 0; i < triggers.Length; i++)
+        {
+            currentTrigger = triggers[i];
+            if(currentTrigger.type == TriggerType.OnEvent && currentTrigger.eventType == EventType.LookAtObject && currentTrigger.triggeringObjects.Length > 0)
+            {
+                for(int j = 0; j < currentTrigger.triggeringObjects.Length; j++)
+                {
+                    if(currentTrigger.triggeringObjects[j] == obj && currentTrigger.isRunning == false)
+                    {
+                        ActivateTrigger(currentTrigger);
+                    }
+                }
+            }
+        }
+    }
+
     IEnumerator RunDialogue(Trigger trigger)
     {
+        trigger.isRunning = true;
         dialogue.text = trigger.textToDisplay;
         yield return new WaitForSeconds(trigger.textDisplayTime);
 
@@ -165,6 +217,7 @@ public class NarrativeTriggerHandler : MonoBehaviour
         dialogue.text = "";
 
         dialogue.CrossFadeAlpha(1, 0f, false);
+        trigger.isRunning = false;
     }
 
     private bool AreaCheck(Trigger triggerToCheck)
@@ -194,10 +247,78 @@ public class NarrativeTriggerHandler : MonoBehaviour
         }
     }
 
+    private void PlayerHitGroundActivation()
+    {
+        //Filter triggers to locate only those that are OnEvent Triggers and are the type of event that activates when the player hits the ground
+        List<Trigger> filteredList = new List<Trigger>();
+        Trigger currentTrigger;
+        for(int i = 0; i < triggers.Length; i++)
+        {
+            currentTrigger = triggers[i];
+            if(currentTrigger.type == TriggerType.OnEvent && currentTrigger.eventType == EventType.PlayerHitGround)
+            {
+                filteredList.Add(currentTrigger);
+            }
+        }
+
+        //No PlayerHitGround Triggers found :(
+        if(filteredList.Count <= 0)
+            return;
+
+        //Pick a random one out of the list to activate
+        currentTrigger = filteredList[Random.Range(0, filteredList.Count)];
+        ActivateTrigger(currentTrigger);
+
+    }
+
+    #region TimeInLevelEvent
+    float timeInLevel = 0;
+    private void TimeInLevelCountStart(UnityEngine.SceneManagement.Scene current, UnityEngine.SceneManagement.Scene next)
+    {
+        timeInLevel = 0;
+        StartCoroutine(TimeInLevelCount());
+    }
+
+    private IEnumerator TimeInLevelCount()
+    {
+        while (true)
+        {
+            for (int i = 0; i < triggers.Length; i++)
+            {
+                if (CheckTimeSinceLevelRequirement(triggers[i]))
+                {
+
+                    ActivateTrigger(triggers[i]);
+                }
+            }
+            timeInLevel += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+    /// <summary>
+    /// Checks if the current total time in the level is greater than 
+    /// </summary>
+    /// <param name="triggerToCheck">The trigger to check</param>
+    /// <returns>boolean true if the requirements are met, false if not</returns>
+    private bool CheckTimeSinceLevelRequirement(Trigger triggerToCheck)
+    {
+        return triggerToCheck.type == TriggerType.OnEvent
+             && triggerToCheck.eventType == EventType.TimeInLevel
+             && timeInLevel >= triggerToCheck.timeInLevelBeforeTrigger
+             && !triggerToCheck.hasRan;
+    }
+    #endregion
+
     #region Getters/Setters
     public string GetTriggerName(int index)
     {
         return triggerNames[index];
+    }
+
+    public float GetFallTime()
+    {
+        return fallTime;
     }
     #endregion
 
