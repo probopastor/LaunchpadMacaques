33b3bebd92f3fc4ca31e785fa1927aff29886f7b
Levi Schoof <levischoof.99@gmail.com>
2021-02-05 00:44:54 -0600

    Rough Version Of Controller Detectioni

---
+++ LaunchpadMacaques_Capstone/Assets/Scripts/Detect Controller/DetectController.cs
@@ -0,0 +1,119 @@
+﻿/* 
+* Launchpad Macaques - Neon Oblivion
+* Levi Schoof
+* DetectController.cs
+* Script handles Detecting Controller Connected/Tells objects
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DetectController : MonoBehaviour
+{
+
+    string[] joySticks;
+    bool controller;
+    A_InputType[] inputSensitive;
+    public static DetectController instance = null;
+   
+
+    private void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
+        StopCoroutine(DetectJoySticks());
+        StartCoroutine(DetectJoySticks());
+    }
+
+
+    /// <summary>
+    /// Loops through Joysticks to check if it can find Xbox Controller
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator DetectJoySticks()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(.5f);
+
+            //Get Joystick Names
+            joySticks = Input.GetJoystickNames();
+
+            
+            // Loops through all the joysticks
+            if (joySticks.Length > 0)
+            {
+                for (int i = 0; i < joySticks.Length; ++i)
+                {
+                    // Will check to see if the Joystick is empty
+                    if (!string.IsNullOrEmpty(joySticks[i]))
+                    {
+                        // Will check to see if the joystick is an xbox controller
+                        if (joySticks[i] == "Controller (Xbox One For Windows)")
+                        {
+                            if (!controller)
+                            {
+                                ControllerConnected();
+                            }
+                        }
+
+                    }
+                    else
+                    {
+                        if (controller)
+                        {
+                            ControllerDisconnected();
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// Informs all inputType objects that controller is connected
+    /// </summary>
+    public void ControllerConnected()
+    {
+        inputSensitive = FindObjectsOfType<A_InputType>();
+        controller = true;
+        foreach (A_InputType sens in inputSensitive)
+        {
+            sens.UpdateUI();
+        }
+    }
+
+    /// <summary>
+    /// Informs all inputType objects that controller is not connected
+    /// </summary>
+    private void ControllerDisconnected()
+    {
+        inputSensitive = FindObjectsOfType<A_InputType>();
+        controller = false;
+        foreach (A_InputType sens in inputSensitive)
+        {
+            sens.UpdateUI();
+        }
+    }
+
+    public bool ControllerEnabled()
+    {
+        return controller;
+    }
+
+
+}
