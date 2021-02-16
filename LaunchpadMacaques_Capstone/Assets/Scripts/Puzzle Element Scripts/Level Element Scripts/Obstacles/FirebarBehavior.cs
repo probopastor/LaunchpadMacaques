83f8e5ffeee166f475c5cb2aa31981cf029556b6
probopastor <43503592+probopastor@users.noreply.github.com>
2021-02-16 10:04:12 -0600

    Made a fire bar
    - Can rotate in a set direction.
    - Can have rotation enabled / disabled
    - Can be set to follow paths determined by Moving Platform.

---
+++ LaunchpadMacaques_Capstone/Assets/Scripts/Puzzle Element Scripts/Level Element Scripts/Obstacles/FirebarBehavior.cs
@@ -0,0 +1,40 @@
+﻿/*
+* Launchpad Macaques - Neon Oblivion
+* William Nomikos
+* FirebarBehavior.cs
+* Script handles Firebar rotation.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirebarBehavior : MonoBehaviour
+{
+    [SerializeField, Tooltip("The speed at which the bar rotates. ")] private float rotationSpeed = 0f;
+    [SerializeField, Tooltip("If enabled, Firebar will move along the path specified by the Moving Platform component. ")] private bool enableMovingPlatform;
+    [SerializeField, Tooltip("Stops Fireball rotation when disabled. ")] private bool turnRotationOn;
+
+    private void Start()
+    {
+        if (enableMovingPlatform)
+        {
+            gameObject.GetComponent<MovingPlatform>().enabled = true;
+            gameObject.GetComponent<LineRenderer>().enabled = true;
+        }
+        else if (!enableMovingPlatform)
+        {
+            gameObject.GetComponent<MovingPlatform>().enabled = false;
+            gameObject.GetComponent<LineRenderer>().enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(turnRotationOn)
+        {
+            transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
