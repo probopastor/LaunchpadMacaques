d5ab485ab2fe41098cff90b46d0a21cd408dd0f0
Jamey Colleen <Enderion@users.noreply.github.com>
2021-02-17 17:07:43 -0600

    Documentation and Making transition image invisible until game runs

--- LaunchpadMacaques_Capstone/Assets/ButtonTransitionManager.cs
+++ LaunchpadMacaques_Capstone/Assets/ButtonTransitionManager.cs
@@ -1,4 +1,12 @@
-﻿using System.Collections;
+﻿/* 
+* Launchpad Macaques 
+* Jamey Colleen 
+* ButtonTransitionManager.cs 
+* Handles animation functionality for Main Menu and other buttons that require use
+* of the animation for making transitions.
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -10,6 +18,9 @@ public class ButtonTransitionManager : MonoBehaviour
 
     [SerializeField, Tooltip("Time to wait to start second half of transition. ")] private int transitionLength = 1;
 
+    
+    [SerializeField, Tooltip("Used to enable animation image.")] private GameObject transitionObject;
+
     public GameObject disable;
     public GameObject enable;
 
@@ -18,25 +29,28 @@ public class ButtonTransitionManager : MonoBehaviour
     void Start()
     {
         transition = GameObject.FindGameObjectWithTag("Transition").GetComponent<Animator>();
+        if (transitionObject == null)
+        {
+            transitionObject = GameObject.FindGameObjectWithTag("Transition");
+        }
+
+        transitionObject.SetActive(true);
+        
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
+    //Gets UI pane to enable from Button
     public void AssignEnable(GameObject enableTarget)
     {
         enable = enableTarget;
     }
 
+    //Gets UI pane to disable from Button
     public void AssignDisable(GameObject disableTarget)
     {
         disable = disableTarget;
     }
 
-
+    //Executes from button in order to switch UI panels via transition
     public void SwitchPanel()
     {
         StartCoroutine(Transition(disable, enable));
@@ -47,6 +61,7 @@ public class ButtonTransitionManager : MonoBehaviour
         StartCoroutine(LevelLoader(levelname));
     }
 
+    //Handles loading level with transition from button
     IEnumerator LevelLoader(string levelName)
     {
         transition.SetTrigger("Start");
@@ -57,6 +72,7 @@ public class ButtonTransitionManager : MonoBehaviour
         SceneManager.LoadScene(levelName);
     }
 
+    //Handles Transition of UI and delay to properly maintain visual cohesion
     IEnumerator Transition(GameObject disable, GameObject enable)
     {
         transition.SetTrigger("Start");
