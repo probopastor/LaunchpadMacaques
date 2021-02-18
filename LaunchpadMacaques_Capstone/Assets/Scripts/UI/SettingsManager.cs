a521d2675e4d26a0f967c98c73ec08a9886f45cf
CJ Green <55067215+Godofwar1300@users.noreply.github.com>
2021-02-18 10:27:36 -0600

    I think things are all set up? See desc. for more details
    I disabled the Volume component in the Main Menu so that it stopped working without taking things out completely.
    
    Everything else that was previously working should be working once again.

--- LaunchpadMacaques_Capstone/Assets/Scripts/UI/SettingsManager.cs
+++ LaunchpadMacaques_Capstone/Assets/Scripts/UI/SettingsManager.cs
@@ -1,6 +1,6 @@
 ﻿/* 
 * Launchpad Macaques - Neon Oblivion
-* Levi Schoof
+* Levi Schoof, CJ Green (Just added two functions for Colorblindness)
 * SettingsManager.CS
 * Script that handles the Options/Settings Screen
 */
@@ -13,6 +13,7 @@ using TMPro;
 using FMODUnity;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using System;
 
 public class SettingsManager : MonoBehaviour
 {
@@ -21,6 +22,7 @@ public class SettingsManager : MonoBehaviour
     [SerializeField, Tooltip("The Resolution Dropdown Box")] TMP_Dropdown resolutionDropdown;
     [SerializeField, Tooltip("The Full Screen Toggle")] Toggle fullScreenToggle;
     [SerializeField, Tooltip("The Graphics Quality Dropdown Box")] TMP_Dropdown graphicsQualityDropdown;
+    [SerializeField, Tooltip("The Colorblind Mode Dropdown Box")] TMP_Dropdown colorblindModeDropdown;
 
     [Header("Volume Sliders")]
     [SerializeField, Tooltip("The Dialouge Volume Slider")] Slider dialougeVolume;
@@ -53,6 +55,7 @@ public class SettingsManager : MonoBehaviour
     // The Deafult variables the sliders will be set to, upon an ititial launch (Player has never played game before)
     #region Deafult Variables
     private int deafultGraphicsQuality = 1;
+    private int defaultColorblindMode = 0;
     private float deafultDialouge = .5f;
     private float deafultMusic = .5f;
     private float deafultSoundEffects = .5f;
@@ -81,12 +84,12 @@ public class SettingsManager : MonoBehaviour
             SetResolutionsDropDown();
             InitialFOV();
             InitialScreenShake();
+            InitialColorblindMode();
 
             DisableStuff();
 
 
         }
-
     }
 
     public void UpdateGameplay()
@@ -111,6 +114,7 @@ public class SettingsManager : MonoBehaviour
         SetResolutionsDropDown();
         InitialFullScreen();
         InitialQuality();
+        InitialColorblindMode();
     }
 
     private void Update()
@@ -430,6 +434,28 @@ public class SettingsManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Called on Start
+    /// WIll Set the Value of the Colorblind Mode to either the default or the "ColorblindMode" Playerprefs
+    /// Weill then apply the value.
+    /// </summary>
+    private void InitialColorblindMode()
+    {
+
+        Debug.Log("This is happening");
+
+        if(PlayerPrefs.HasKey("ColorblindMode"))
+        {
+            colorblindModeDropdown.SetValueWithoutNotify(PlayerPrefs.GetInt("ColorblindMode"));
+            SetColorblindMode(PlayerPrefs.GetInt("ColorblindMode"));
+        }
+        else
+        {
+            SetColorblindMode(defaultColorblindMode);
+            colorblindModeDropdown.SetValueWithoutNotify(defaultColorblindMode);
+        }
+    }
+
     #endregion
 
     // Is called when the player changes a setting on the options menu
@@ -549,6 +575,15 @@ public class SettingsManager : MonoBehaviour
         QualitySettings.SetQualityLevel(qualityLevel);
 
         PlayerPrefs.SetInt("QualityLevel", qualityLevel);
+    }
+
+    /// <summary>
+    /// Will be called when the player chooses a new Colorblind Mode.
+    /// </summary>
+    /// <param name="colorblindMode"></param>
+    public void SetColorblindMode(int colorblindMode)
+    {
+        PlayerPrefs.SetInt("ColorblindMode", colorblindMode);
     }
 
     /// <summary>
