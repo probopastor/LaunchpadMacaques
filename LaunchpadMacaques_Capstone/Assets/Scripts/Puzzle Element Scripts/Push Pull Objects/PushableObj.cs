c3df584f2035be85976af66ba122bc05c65ca65c
Jamey Colleen <Enderion@users.noreply.github.com>
2021-02-24 15:40:09 -0600

    Revert "Fixed particles"
    This reverts commit 0efa1e6beac77615d54716cf6dc8f880ccfa7368.

--- LaunchpadMacaques_Capstone/Assets/Scripts/Puzzle Element Scripts/Push Pull Objects/PushableObj.cs
+++ LaunchpadMacaques_Capstone/Assets/Scripts/Puzzle Element Scripts/Push Pull Objects/PushableObj.cs
@@ -62,7 +62,6 @@ public class PushableObj : MonoBehaviour
     private ParticleSystem.MinMaxCurve particleStartingSize;
     private PushPullObjects pushPull;
 
-    private ParticleTrigger Trigger;
     private GameObject outlineObj;
 
     private float cubeRadius;
@@ -82,8 +81,6 @@ public class PushableObj : MonoBehaviour
         pushPull = FindObjectOfType<PushPullObjects>();
         respawnRef = GetComponent<CubeRespawn>();
         CreateDecalAndLine();
-
-        Trigger = this.GetComponent<ParticleTrigger>();
 
         grav = this.GetComponent<Gravity>();
         cc = FindObjectOfType<CollectibleController>();
@@ -500,8 +497,6 @@ public class PushableObj : MonoBehaviour
         if (onOff)
         {
             particles.Play();
-            Trigger.Trigger();
-            
         }
 
         else
