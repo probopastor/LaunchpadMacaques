d66522f650d2fa4466a9e03ca4f0e669419eaa6d
CJ Green <55067215+Godofwar1300@users.noreply.github.com>
2021-04-13 11:50:36 -0500

    Started Warning Screen Stuff

---
+++ LaunchpadMacaques_Capstone/Assets/Scripts/Splash Screen Stuff/SplashScreen.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using TMPro;
+
+public class SplashScreen : MonoBehaviour
+{
+
+    [SerializeField]
+    private string splashScreenName;
+
+    [SerializeField]
+    private float waitTime;
+
+    [SerializeField]
+    private TextMeshProUGUI text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public string GetSplashScreenName()
+    {
+        return splashScreenName;
+    }
+
+    public float GetWaitTime()
+    {
+        return waitTime;
+    }
+
+    public TextMeshProUGUI GetText()
+    {
+        return text;
+    }
+
+}
---
+++ LaunchpadMacaques_Capstone/Assets/Scripts/Splash Screen Stuff/SplashScreenManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+
+public class SplashScreenManager : MonoBehaviour
+{
+
+    private SplashScreen[] splashScreens;
+
+    private void Awake()
+    {
+        splashScreens = FindObjectsOfType<SplashScreen>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (splashScreens.Length <= 1)
+        {
+            StartCoroutine(WaitTime(splashScreens[0].GetWaitTime()));
+        }
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            SkipWarning();
+        }
+    }
+
+    private IEnumerator WaitTime(float waitTime)
+    {
+
+        yield return new WaitForSeconds(waitTime);
+        SceneManager.LoadScene("MainMenu");
+
+    }
+
+    public void SkipWarning()
+    {
+
+        StopAllCoroutines();
+        SceneManager.LoadScene("MainMenu");
+
+    }
+
+
+}
