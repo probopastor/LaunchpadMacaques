ea24a98dfe342a26dae0e0040f859e62f765de2d
CJ Green <55067215+Godofwar1300@users.noreply.github.com>
2021-04-18 16:51:32 -0500

    Current Splash Screen Stuff
    I added some fade code to the splash screen elements. Test stuff.

--- LaunchpadMacaques_Capstone/Assets/Scripts/Splash Screen Stuff/SplashScreen.cs
+++ LaunchpadMacaques_Capstone/Assets/Scripts/Splash Screen Stuff/SplashScreen.cs
@@ -2,24 +2,35 @@
 using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class SplashScreen : MonoBehaviour
 {
 
     [SerializeField]
-    private string splashScreenName;
+    private string splashScreenName = "Default";
 
     [SerializeField]
-    private float waitTime;
+    private float waitTime = 5.0f;
 
     [SerializeField]
     private TextMeshProUGUI text;
 
+    private bool isFaddingDone = false;
+
+    [SerializeField]
+    private List<GameObject> faddingObjects = new List<GameObject>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //StartCoroutine(FadeTo(faddingObjects[0].GetComponent<Image>().color, 1f, 5f));
+        //StartCoroutine(FadeTo(faddingObjects[1].GetComponent<TextMeshProUGUI>().color, 0f, 5f));
+
+        //var TempColor = faddingObjects[1].GetComponent<TextMeshProUGUI>().color;
+        //TempColor.a = 0.5f;
+        //faddingObjects[1].GetComponent<TextMeshProUGUI>().color = TempColor;
     }
 
     // Update is called once per frame
@@ -28,6 +39,39 @@ public class SplashScreen : MonoBehaviour
         
     }
 
+    public IEnumerator FadeTo(Color imageColor, float targetOpacity, float duration)
+    {
+
+        isFaddingDone = false;
+
+        // How many seconds we've been fading for.
+        float elapsedTime = 0;
+
+        // Cached reference of Image color.
+        Color tempColor = imageColor;
+
+        // Referance to starting opacity.
+        float startingOpacity = tempColor.a;
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+
+            float blend = Mathf.Clamp01(elapsedTime / duration);
+
+            tempColor.a = Mathf.Lerp(startingOpacity, targetOpacity, blend);
+
+            imageColor = tempColor;
+
+            yield return null;
+
+            if (elapsedTime >= duration)
+            {
+                StopCoroutine("FadeTo");
+                isFaddingDone = true;
+            }
+        }
+    }
 
     public string GetSplashScreenName()
     {
--- LaunchpadMacaques_Capstone/Assets/Scripts/Splash Screen Stuff/SplashScreenManager.cs
+++ LaunchpadMacaques_Capstone/Assets/Scripts/Splash Screen Stuff/SplashScreenManager.cs
@@ -2,35 +2,81 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.Rendering;
+using UnityEngine.Assertions.Must;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SplashScreenManager : MonoBehaviour
 {
 
     private SplashScreen[] splashScreens;
 
+    private Image mainMenuBackground;
+
+    [SerializeField]
+    private bool isFaddingDone = false;
+
     private void Awake()
     {
         splashScreens = FindObjectsOfType<SplashScreen>();
+        mainMenuBackground = FindObjectOfType<Canvas>().transform.GetChild(0).GetComponent<Image>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        // Fade things in at start.
+        StartCoroutine(FadeTo(mainMenuBackground, 1f, 5f));
+    }
 
-        if (splashScreens.Length <= 1)
+    private void Update()
+    {
+
+        Debug.Log(isFaddingDone);
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            SkipWarning();
+        }
+
+        if (splashScreens.Length <= 1 && isFaddingDone)
         {
             StartCoroutine(WaitTime(splashScreens[0].GetWaitTime()));
         }
 
     }
 
-    private void Update()
+    public IEnumerator FadeTo(Image imageColor, float targetOpacity, float duration)
     {
-        if (Input.GetKeyDown(KeyCode.P))
+
+        isFaddingDone = false;
+
+        // How many seconds we've been fading for.
+        float elapsedTime = 0;
+        
+        // Cached reference of Image color.
+        Color tempColor = imageColor.color;
+
+        // Referance to starting opacity.
+        float startingOpacity = tempColor.a;
+
+        while(elapsedTime < duration)
         {
-            SkipWarning();
+            elapsedTime += Time.deltaTime;
+
+            float blend = Mathf.Clamp01(elapsedTime / duration);
+
+            tempColor.a = Mathf.Lerp(startingOpacity, targetOpacity, blend);
+
+            mainMenuBackground.color = tempColor;
+
+            yield return null;
+
+            if(elapsedTime >= duration)
+            {
+                StopCoroutine("FadeTo");
+                isFaddingDone = true;
+            }
         }
     }
 
