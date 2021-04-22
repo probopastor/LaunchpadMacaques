440673d0545202c2bd4e1eb7bf9c7b8a857cf600
Jamey Colleen <enderionthehunter@gmail.com>
2021-04-22 09:06:51 -0500

    Step 3 done

---
+++ LaunchpadMacaques_Capstone/Assets/DisableMesh.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableMesh : MonoBehaviour
+{
+
+    MeshRenderer Cube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cube = GetComponent<MeshRenderer>();
+        Cube.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
