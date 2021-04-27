b1b7164fdf44cae94e1c1174296b3f0d06573899
Levi Schoof <levischoof.99@gmail.com>
2021-04-27 17:50:45 -0500

    Fix How To play screens

---
+++ LaunchpadMacaques_Capstone/Assets/HowToPlay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HowToPlay : A_InputType
+{
+    [SerializeField] GameObject keyBoard;
+    [SerializeField] GameObject controller;
+
+    public override void ChangeUI()
+    {
+        if (controllerDetected)
+        {
+            keyBoard.SetActive(false);
+            controller.SetActive(true);
+        }
+
+        else
+        {
+            controller.SetActive(false);
+            keyBoard.SetActive(true);
+        }
+    }
+
+    private new void OnEnable()
+    {
+        base.OnEnable();
+        ChangeUI();
+    }
+}
