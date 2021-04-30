24865c49b7558ffe017f2da80778cfa350635be5
Cole Kersting <48395845+Ckersting@users.noreply.github.com>
2021-04-30 16:54:57 -0500

    Commented out broken line

--- LaunchpadMacaques_Capstone/Assets/Scripts/Wizard Interactions/WizardInteraction.cs
+++ LaunchpadMacaques_Capstone/Assets/Scripts/Wizard Interactions/WizardInteraction.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Net;
 using System.Runtime.CompilerServices;
 using System.Xml.Schema;
-using UnityEditorInternal;
 using UnityEngine;
 
 public class WizardInteraction : MonoBehaviour
