13f0e088bcba0c5d2a5a07683441d78894fe64f6
CJ Green <55067215+Godofwar1300@users.noreply.github.com>
2021-04-30 10:33:11 -0500

    Added Interaction package onto branch

---
+++ LaunchpadMacaques_Capstone/Assets/Scripts/Wizard Interactions/Portal.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+
+    public enum PortalStates {INACTIVE, OPEN, CLOSE };
+
+    public PortalStates portalState = PortalStates.INACTIVE;
+
+    private int wizardCollisions = 0;
+
+
+    private void Update()
+    {
+        // Check Portal State...
+        CheckPortalState(portalState);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Wizard")
+        {
+            wizardCollisions++;
+            Debug.Log("The wizard has collided with the portal! with a collision count of: " + wizardCollisions);
+        }
+    }
+
+    private void CheckPortalState(PortalStates currentPortalState)
+    {
+        if(currentPortalState == PortalStates.INACTIVE)
+        {
+            gameObject.SetActive(false);
+        }
+        else if(currentPortalState == PortalStates.OPEN)
+        {
+            gameObject.SetActive(true);
+        }
+        else if(currentPortalState == PortalStates.CLOSE)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    #region Getters/Setters
+    public PortalStates PortalStatesReference
+    {
+        get
+        {
+            return portalState;
+        }
+        set
+        {
+            portalState = value;
+        }
+    }
+
+    public int WizardCollisions
+    {
+        get
+        {
+            return wizardCollisions;
+        }
+        set
+        {
+            wizardCollisions = value;
+        }
+    }
+
+    #endregion
+
+}
---
+++ LaunchpadMacaques_Capstone/Assets/Scripts/Wizard Interactions/WizardInteractionsManager.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Schema;
+using UnityEngine;
+
+public class WizardInteractionsManager : MonoBehaviour
+{
+
+    private Vector3 wizardPosition = new Vector3(0, 0, 0);
+    private bool isWizardTalking = false;
+
+    private bool wizardIntroIsPlaying = false;
+    private bool wizardOutroIsPlaying = false;
+
+    [SerializeField]
+    private List<string> interactionNames = new List<string>();
+
+    private GameObject wizardGameObject;
+    private WizardInteraction wizardInteraction;
+    private Portal wizardPortal;
+
+    private NarrativeTriggerHandler narrativeHandler;
+    private Matt_PlayerMovement movementScript;
+
+    private void Awake()
+    {
+        wizardGameObject = GameObject.FindGameObjectWithTag("Wizard");
+        wizardPortal = FindObjectOfType<Portal>();
+        narrativeHandler = FindObjectOfType<NarrativeTriggerHandler>();
+        wizardInteraction = FindObjectOfType<WizardInteraction>();
+        movementScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Matt_PlayerMovement>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name != "Hub")
+        {
+            Debug.Log("We are not in the hub");
+
+            if (wizardInteraction.InteractionName == interactionNames[0]) // UnityEngine.SceneManagement.SceneManager.GetActiveScene().name != "Hub" && 
+            {
+
+                IsWizardIntroPlaying = true;
+                StartCoroutine(WizardIntro());
+
+            }
+            else if (wizardInteraction.InteractionName == interactionNames[2])
+            {
+                IsWizardIntroPlaying = true;
+                StartCoroutine(WizardIntro());
+            }
+
+            //IsWizardIntroPlaying = true;
+            //StartCoroutine(WizardIntro());
+            //StartCoroutine(wizardInteraction.MoveWizardBackwards());
+        }
+        else if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "Hub")
+        {
+            Debug.Log("We are in the Hub");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        LimitMovement();
+
+        if (wizardInteraction.InteractionName == interactionNames[0] || wizardInteraction.InteractionName == interactionNames[2])
+        {
+            if (narrativeHandler.CurrentDialogueLine != null)
+            {
+
+                if (narrativeHandler.CurrentDialogueLine.GetLineType() == Dialogue.Line.Type.NarrationLine)
+                {
+
+                    Debug.Log("The current line of dialogue is: " + narrativeHandler.CurrentDialogueLine.text);
+                    Debug.Log("The current line of dialogue is: " + narrativeHandler.CurrentDialogueLine.GetLineType());
+
+                    if(IsWizardTalking)
+                    {
+                        wizardInteraction.WizardAnimator.SetBool("isTalking", false);
+                    }
+
+
+                    IsWizardOutroPlaying = true;
+                    StartCoroutine(WizardOutro());
+                }
+
+            }
+        }
+
+    }
+
+    private IEnumerator WizardIntro()
+    {
+
+        if (IsWizardIntroPlaying)
+        {
+            // Opens the portal and then wait two seconds.
+            wizardPortal.PortalStatesReference = Portal.PortalStates.OPEN;
+
+            yield return new WaitForSeconds(5f);
+
+
+            // Start Moving the Wizard
+            StartCoroutine(wizardInteraction.MoveWizardForward());
+
+            IsWizardIntroPlaying = false;
+
+        }
+
+        StopCoroutine(WizardIntro());
+        Debug.Log("Wizard Intro has stopped!");
+
+    }
+
+    private IEnumerator WizardOutro()
+    {
+
+        if (IsWizardOutroPlaying)
+        {
+
+            StartCoroutine(wizardInteraction.MoveWizardBackwards());
+
+
+            if (WizardPortalReference.WizardCollisions == 2)
+            {
+                wizardGameObject.SetActive(false);
+
+                yield return new WaitForSeconds(1f);
+
+                wizardPortal.PortalStatesReference = Portal.PortalStates.CLOSE;
+                wizardInteraction.StopAllCoroutines();
+
+                yield return null;
+            }
+
+
+        }
+
+        StopCoroutine(WizardOutro());
+    }
+
+    private void LimitMovement()
+    {
+
+        if (IsWizardIntroPlaying)
+        {
+            movementScript.SetPlayerCanMove(false);
+        }
+
+    }
+
+    #region Getters and Setters
+    public bool IsWizardTalking
+    {
+        get
+        {
+            return isWizardTalking;
+        }
+        set
+        {
+            isWizardTalking = value;
+        }
+    }
+
+    public bool IsWizardIntroPlaying
+    {
+        get
+        {
+            return wizardIntroIsPlaying;
+        }
+        set
+        {
+            wizardIntroIsPlaying = value;
+        }
+    }
+
+    public bool IsWizardOutroPlaying
+    {
+        get
+        {
+            return wizardOutroIsPlaying;
+        }
+        set
+        {
+            wizardOutroIsPlaying = value;
+        }
+    }
+
+    public Portal WizardPortalReference
+    {
+        get
+        {
+            return wizardPortal;
+        }
+    }
+
+    #endregion 
+
+}
