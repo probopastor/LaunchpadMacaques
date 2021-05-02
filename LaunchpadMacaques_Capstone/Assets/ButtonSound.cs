61566c6ad664d9dee92c2920a0209fe21abd9642
Levi Schoof <levischoof.99@gmail.com>
2021-05-02 14:14:59 -0500

    Fix issue with sounds playing whenever you click

---
+++ LaunchpadMacaques_Capstone/Assets/ButtonSound.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using FMODUnity;
+public class ButtonSound : MonoBehaviour
+{
+    EventSystem es;
+    private GameObject lastSelectObject;
+    private StudioEventEmitter eventEmiter;
+
+    private void Awake()
+    {
+        es = FindObjectOfType<EventSystem>();
+        eventEmiter = this.GetComponent<StudioEventEmitter>();
+    }
+
+    private void Start()
+    {
+        lastSelectObject = es.currentSelectedGameObject;
+    }
+
+    private void Update()
+    {
+        if (lastSelectObject)
+        {
+            if (es.currentSelectedGameObject != lastSelectObject)
+            {
+                lastSelectObject = es.currentSelectedGameObject;
+            }
+        }
+
+        else
+        {
+            lastSelectObject = es.currentSelectedGameObject;
+        }
+
+    }
+
+    public void Selected()
+    {
+        if (lastSelectObject)
+        {
+            if (lastSelectObject != this.gameObject)
+            {
+                eventEmiter.Play();
+            }
+        }
+
+    }
+}
